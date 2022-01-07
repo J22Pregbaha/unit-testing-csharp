a37b063703423af4ef6fafbc9881aea6abbb4dc4
Joshua Pregbaha <jpregbaha@gmail.com>
2022-01-07 13:15:10 +0000

    first commit

---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4229509-707e-4bdb-a823-b6ad029894cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+            var user = new User { IsAdmin = true };
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsSameAsUserWhoMadeReservation_ReturnTrue()
+        {
+            // Arrange
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsNotAdminOrSameAsUserWhoMadeReservation_ReturnFalse()
+        {
+            // Arrange
+            var user = new User { IsAdmin = true };
+            var reservation = new Reservation { MadeBy = user };
+            var unauthorized_user = new User();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(unauthorized_user);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
