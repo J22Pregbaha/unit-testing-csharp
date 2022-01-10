70410eb3676541fac0cb0c1a93af414c1e39f77e
Jumping Boy <jpregbaha@gmail.com>
2022-01-10 11:18:51 +0100

    Testing Void methods

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            
+            logger.Log("a");
+            
+            Assert.That(logger.LastError == "a");
+        }
+    }
+}
