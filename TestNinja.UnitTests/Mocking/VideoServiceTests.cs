bcef24d93e8715cdabe837a8564079cf7c58d7d1
Jumping Boy <jpregbaha@gmail.com>
2022-01-11 15:49:49 +0100

    Dependency injection vie Properties

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -9,9 +9,12 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService();
+            var service = new VideoService
+            {
+                FileReader = new FakeFileReader()
+            };
 
-            var result = service.ReadVideoTitle(new FakeFileReader());
+            var result = service.ReadVideoTitle();
             
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
