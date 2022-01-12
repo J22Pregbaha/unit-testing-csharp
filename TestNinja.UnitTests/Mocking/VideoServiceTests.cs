3ab18d14e34efae0de58bee11cd933597e61c001
Jumping Boy <jpregbaha@gmail.com>
2022-01-12 13:27:45 +0100

    Introduction to Mock Testing using Moq

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,3 +1,4 @@
+using Moq;
 using NUnit.Framework;
 using TestNinja.Mocking;
 
@@ -6,12 +7,22 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
+        private Mock<IFileReader> _fileReader;
+        private VideoService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _service = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new FakeFileReader());
+            _fileReader.Setup(fR => fR.Read("video.txt")).Returns("");
 
-            var result = service.ReadVideoTitle();
+            var result = _service.ReadVideoTitle();
             
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
